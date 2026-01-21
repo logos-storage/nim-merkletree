9a43c44c7d2f9d6834c4c39e6463abee426d045c
E M <5089238+emizzle@users.noreply.github.com>
2026-01-21 20:31:17 +1100

    remove unused

--- src/merkletree/utils/digest.nim
+++
@@ -1,7 +0,0 @@
-from pkg/libp2p import MultiHash
-
-func digestBytes*(mhash: MultiHash): seq[byte] =
-  ## Extract hash digestBytes
-  ##
-
-  mhash.data.buffer[mhash.dpos ..< mhash.dpos + mhash.size]
